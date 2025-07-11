d5fb9ee2f758b675eeb89883eb8904f59c2ab262
Alexhohner2024 <alex.babaian2008@gmail.com>
2025-07-11 16:06:36 +0300

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,1 +1,7 @@
-
+export default function handler(req, res) {
+  console.log('Тестовый запрос принят');
+  res.json({
+    message: 'SGTas QR Generator работает!',
+    timestamp: new Date().toISOString()
+  });
+}
